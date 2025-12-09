78486f07f73ee8284c380067f830fe651ad37b41
rickyriosp <riki.rios91@gmail.com>
2025-12-09 18:04:27 -0500

    merged portfolio & resume html

---
+++ frontend/assets/js/dark-mode.js
@@ -0,0 +1,53 @@
+"use strict";
+
+/* Dependency: js-cookie plugin - Ref: https://github.com/js-cookie/js-cookie */
+
+const modeToggler = document.getElementById('darkmode');
+const documentBody = document.getElementsByTagName('body')[0];
+
+
+function setThemeFromCookie() {
+	// Check if the cookie is set 
+	if (typeof Cookies.get('mode') !== "undefined" ) {
+		documentBody.classList.add('dark-mode');
+		modeToggler.checked = true; // toggle change
+		
+		console.log('Cookie: dark mode' );
+		
+	} else {
+		documentBody.classList.remove('dark-mode');
+		modeToggler.checked = false; // toggle change
+		
+		console.log('Cookie: light mode' );
+	}
+}
+
+
+setThemeFromCookie();
+
+
+modeToggler.addEventListener('change', () => {
+	
+	console.log(modeToggler.checked);
+	
+    if (modeToggler.checked) {
+		documentBody.classList.add('dark-mode');
+		//Set cookies for 7 days 
+		Cookies.set('mode', 'dark-mode', { expires: 7 });
+		
+		console.log ('change to dark mode');
+		
+	} else {
+		documentBody.classList.remove('dark-mode');
+		//Remove cookies
+		Cookies.remove('mode');
+		
+		console.log ('change to light mode');
+	}
+});
+
+
+// Dark mode by default on first open
+modeToggler.checked = true;
+const customEvent = new Event('change');
+modeToggler.dispatchEvent(customEvent);
---
+++ frontend/php/contact.php
@@ -0,0 +1,96 @@
+<?php
+
+$errors = "";
+
+// Name
+if (empty($_POST["cname"])) {
+    $errors = "Name is required ";
+} else {
+    $name = $_POST["cname"];
+}
+
+// Email
+if (empty($_POST["cemail"])) {
+    $errors .= "Email is required ";
+} else {
+    $email = $_POST["cemail"];
+}
+
+// Subject
+if (empty($_POST["csubject"])) {
+    $errors = "Subject is required ";
+} else {
+    $subject = $_POST["csubject"];
+}
+
+// Message
+if (empty($_POST["cmessage"])) {
+    $errors .= "Message is required ";
+} else {
+    $message = $_POST["cmessage"];
+}
+
+// Form validation
+if(!empty($name) && !empty($email) && !empty($subject) && !empty($message)){
+
+    // reCAPTCHA validation
+    if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])) {
+
+        // Google secret API
+        $secretAPIkey = getenv("reCaptchaAPIkey");
+
+        // reCAPTCHA response verification
+        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secretAPIkey.'&response='.$_POST['g-recaptcha-response']);
+
+        // Decode JSON data
+        $response = json_decode($verifyResponse);
+        if($response->success){
+            // Email recipient
+            $toMail = "ricardo@riosr.com";
+
+            // Prepare email body text
+            $Body = "";
+            $Body .= "Name: ";
+            $Body .= $name;
+            $Body .= "\n";
+            $Body .= "Email: ";
+            $Body .= $email;
+            $Body .= "\n";
+            $Body .= "Message: ";
+            $Body .= $message;
+            $Body .= "\n";
+
+            // Send email
+            $success = mail($toMail, $subject, $Body, "From:"."contact@riosr.com");
+
+            // Redirect to success page
+            if ($success && $errors == ""){
+                echo 'success';
+            }
+            else{
+                echo 'errors sending email';
+                echo $errors;
+            }
+        } else {
+            $response = array(
+                "status" => "alert-danger",
+                "message" => "Robot verification failed, please try again."
+            );
+            echo implode(", ", $response);
+        }       
+    } else{ 
+        $response = array(
+            "status" => "alert-danger",
+            "message" => "Plese check on the reCAPTCHA box."
+        );
+        echo implode(", ", $response);
+    } 
+}  else{ 
+    $response = array(
+        "status" => "alert-danger",
+        "message" => "All the fields are required."
+    );
+    echo implode(", ", $response);
+}
+
+?>
