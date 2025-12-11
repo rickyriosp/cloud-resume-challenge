7ac024b587f1f0487088304cbda9710954b2a594
rickyriosp <riki.rios91@gmail.com>
2025-12-10 21:44:11 -0500

    Fix S3 bucket policy resource to use bucket ID instead of ARN

--- aws/s3.tf
+++ aws/s3.tf
@@ -37,7 +37,7 @@ resource "aws_s3_bucket_public_access_block" "frontend" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3control_bucket_policy" "frontend" {
-  bucket = aws_s3_bucket.frontend.arn
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
   policy = data.aws_iam_policy_document.frontend_origin_bucket_policy.json
 }
