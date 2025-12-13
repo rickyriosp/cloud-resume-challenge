c1bfe744e0b025aeec0c941e9d8efd8b9931ea1e
Ricky Rios <riki.rios91@gmail.com>
2025-12-13 05:23:05 +0000

    Add IAM policy for DynamoDB access and attach to Lambda role

--- aws/terraform/iam.tf
+++ aws/terraform/iam.tf
@@ -29,3 +29,31 @@ resource "aws_iam_role_policy_attachment" "apigateway_cloudwatch" {
   policy_arn = aws_iam_policy.cloudwatch_access.arn
   role       = aws_iam_role.viewcounter_apigateway.name
 }
+
+# IAM policy for DynamoDB access
+data "aws_iam_policy_document" "dynamodb_role" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:BatchGetItem",
+      "dynamodb:BatchWriteItem",
+      "dynamodb:DescribeTable",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:Query",
+      "dynamodb:Scan",
+      "dynamodb:UpdateItem",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "dynamodb_access" {
+  name = "viewcounter-dynamodb-access"
+  policy = data.aws_iam_policy_document.dynamodb_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb" {
+  policy_arn = aws_iam_policy.dynamodb_access.arn
+  role       = aws_iam_role.viewcounter_lambda.name
+}
